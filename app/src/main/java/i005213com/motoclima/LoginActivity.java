c5c272c3c91b6732de50002529584c0142a536b6
JuanDiego <j>
2017-05-10 17:43:26 -0500

    CAmbiosunu

--- app/src/main/java/i005213com/motoclima/LoginActivity.java
+++ app/src/main/java/i005213com/motoclima/LoginActivity.java
@@ -18,17 +18,10 @@ import android.widget.Toast;
 import java.io.IOException;
 import java.util.List;
 
-import i005213com.motoclima.Adapters.UserAdapter;
-import i005213com.motoclima.Models.User;
-import i005213com.motoclima.Parcer.Json;
 import i005213com.motoclima.Views.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    ProgressBar loader;
-    RecyclerView myRecycler;
-    List<User> myUser;
-    UserAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,98 +29,11 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
-
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        myRecycler.setLayoutManager(linearLayoutManager);
     }
 
-    public void goCreateAccount(View view){
+    public void goCreateAccount(View view) {
         Intent intent = new Intent(this, CreateAccountActivity.class);
         startActivity(intent);
-    }
 
-    // Metodo para validar la conexion a internet
-    public Boolean isOnLine(){
-        ConnectivityManager connec = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netInfo = connec.getActiveNetworkInfo();
-
-        if (netInfo != null && netInfo.isConnectedOrConnecting()){
-            return true;
-        }else {
-            return false;
-        }
-    }
-
-    // Medodo para manejar el evento del item del menu
-    public void onClickButton(){
-        if (isOnLine()){
-            MyTask task = new MyTask();
-            task.execute("https://jsonplaceholder.typicode.com/users");
-        }else {
-            Toast.makeText(this, "Sin conexión", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    // Tarea asincrona para obtener los datos desde internet
-    private class MyTask extends AsyncTask<String, String, String> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            loader.setVisibility(View.VISIBLE);
-
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-
-            String content = null;
-            try {
-                content = HttpManager.getData(params[0]);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return content;
-
-        }
-
-        @Override
-        protected void onProgressUpdate(String... values) {
-            super.onProgressUpdate(values);
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-
-            try {
-                myUser = Json.parserJsonUser(s);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            cargarDatos();
-            loader.setVisibility(View.GONE);
-
-        }
-    }
-
-    public void cargarDatos(){
-
-        myAdapter = new UserAdapter(getApplicationContext(),myUser);
-
-        myRecycler.setAdapter(myAdapter);
-
-    }
-
-    // Metodo para para inicializar el menu en el Toolbar
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main,menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        onClickButton();
-        return super.onOptionsItemSelected(item);
     }
 }
