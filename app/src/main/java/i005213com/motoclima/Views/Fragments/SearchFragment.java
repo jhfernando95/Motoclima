74161c061752c170c28526b688b734b993711330
jhony <jhfernando@gmail.com>
2017-05-22 21:42:01 -0500

    22/05/2017

--- app/src/main/java/i005213com/motoclima/Views/Fragments/SearchFragment.java
+++ app/src/main/java/i005213com/motoclima/Views/Fragments/SearchFragment.java
@@ -6,25 +6,68 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import i005213com.motoclima.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment implements OnMapReadyCallback {
+    GoogleMaps m_map;
+    boolean mapReady = false;
 
-
-    public SearchFragment() {
-        // Required empty public constructor
-    }
-
+    static final CameraPosition PASTO = CameraPosition.builder()
+            .target(new LatLng(1.2086284, -77.2779443))
+            .zoom(15)
+            .bearing(90)
+            .tilt(45)
+            .build();
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_search);
+
+
+        Button btnNormal = (Button) findViewById(R.id.id_btn_seattle);
+        btnNormal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                motoclima(SEATTLE);
+                // m_map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            }
+        });
+
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mapReady = true;
+        m_map = googleMap;
+        //LatLng pasto = new LatLng(1.2086284 , -77.2779443);
+        //CameraPosition target = CameraPosition.builder().target(pasto).zoom(15).build();
+        //m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+        motoclima(PASTO);
+
+    }
+    public  void motoclima(CameraPosition perro) {
+
+        m_map.animateCamera(CameraUpdateFactory.newCameraPosition(perro), 2000, null);
+
+    }
 }
+
+
