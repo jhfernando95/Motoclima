824dbc34767d536782bfe70cc150da79d465cf08
JuanDiego <j>
2017-06-08 12:20:42 -0500

    looza

---
+++ app/src/main/java/i005213com/motoclima/Data/DataUser.java
@@ -0,0 +1,22 @@
+package i005213com.motoclima.Data;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import i005213com.motoclima.Helper.HelperUser;
+import i005213com.motoclima.Models.User;
+
+/**
+ * Created by JuanDiego on 8/06/17.
+ */
+
+public class DataUser {
+
+
+}
